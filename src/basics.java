200faa684359b4c5c66f34cbcb07c26a8e16cdc9
rizcube <rizcubeservices@gmail.com>
2020-06-08 15:49:27 +0100

    created payload.java class file with  public static String AddPlace(), this method returns the body and  is then called in basics.java file

--- src/basics.java
+++ src/basics.java
@@ -2,6 +2,8 @@ import io.restassured.RestAssured;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+import files.payload;
+
 public class basics {
 
 	public static void main(String[] args) {
@@ -15,24 +17,12 @@ public class basics {
 		
 		RestAssured.baseURI = "https://rahulshettyacademy.com";
 		given().log().all().queryParam("Key", "qaclick123").header("Content-Type","application/json")
-		.body("{\n" + 
-				"  \"location\": {\n" + 
-				"    \"lat\": -38.383494,\n" + 
-				"    \"lng\": 33.427362\n" + 
-				"  },\n" + 
-				"  \"accuracy\": 50,\n" + 
-				"  \"name\": \"Princess Castle 3\",\n" + 
-				"  \"phone_number\": \"(+91) 983 893 3937\",\n" + 
-				"  \"address\": \"Spiritual Landing place\",\n" + 
-				"  \"types\": [\n" + 
-				"    \"shoe park\",\n" + 
-				"    \"shop\"\n" + 
-				"  ],\n" + 
-				"  \"website\": \"http://google.com\",\n" + 
-				"  \"language\": \"French-IN\"\n" + 
-				"}").when().post("maps/api/place/add/json")
+		.body(payload.AddPlace()).when().post("maps/api/place/add/json")
 		.then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"))
 		.header("server", "Apache/2.4.18 (Ubuntu)");
+		
+		//Add place -> update place with new Address -> get place to validate if new addres is present in response.
+		
 	}
 
 }
