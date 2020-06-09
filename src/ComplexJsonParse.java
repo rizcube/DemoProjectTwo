a72cb242d2286b7c511aefb9d43d8c707f528341
rizcube <rizcubeservices@gmail.com>
2020-06-09 15:06:03 +0100

    added for loop to retrieve names of all the title courses

--- src/ComplexJsonParse.java
+++ src/ComplexJsonParse.java
@@ -13,6 +13,20 @@ public class ComplexJsonParse {
 		// purchase amount
 		int totalAmount = js.getInt("dashboard.purchaseAmount");
 		System.out.println(totalAmount);
+	
+	
+		// print Title of the first course
+		String title = js.getString("courses[0].title");
+		System.out.println(title);
+	
+	
+		//print title and price for each course available in the json
+		
+		for(int i=0; i<count; i++) {
+			String courseTitles = js.get("courses["+i+"].title");
+			System.out.println(courseTitles);
+		}
+		
 	}
 
 }
