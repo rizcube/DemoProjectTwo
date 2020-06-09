070477fc14f0d02ec6dc24100600eec77b3e4f62
rizcube <rizcubeservices@gmail.com>
2020-06-09 14:24:32 +0100

    using courses.size(), got the number of courses available in the json response

--- src/ComplexJsonParse.java
+++ src/ComplexJsonParse.java
@@ -6,6 +6,9 @@ public class ComplexJsonParse {
 	public static void main(String[] args) {
 		
 		JsonPath js = new JsonPath(payload.CoursePrice());
+		// Print no of courses returned by API
+		int count = js.getInt("courses.size()");
+		System.out.println(count);
 	}
 
 }
