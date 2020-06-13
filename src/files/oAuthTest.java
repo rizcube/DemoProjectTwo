bf0c9a2ee7bc3972a5895e99e28a7d2337b741e5
rizcube <rizcubeservices@gmail.com>
2020-06-13 14:09:18 +0100

    query params added with its values except the value of code

--- src/files/oAuthTest.java
+++ src/files/oAuthTest.java
@@ -1,12 +1,28 @@
 package files;
 import static io.restassured.RestAssured.given;
 
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
 public class oAuthTest {
 	
 	public static void main (String[] args) 
 	{
-		String response = given().queryParam("access_token", "")
-		.when()
+		String accessTokenResponse  =given().queryParams("code","")
+		.queryParams("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
+		.queryParams("client_secret", "erZOWM9g3UtwNRj340YYaK_W")
+		.queryParams("redirect_uri", "https://rahulshettyacademy.com/getCourse.php")
+		.queryParams("grant_type", "authorization_code")
+		
+		.when().log().all()
+		.post("https://www.googleapis.com/oauth2/v4/token").asString();
+		
+		JsonPath js = new JsonPath(accessTokenResponse);
+		String accessToken = js.getString("access_token");
+		
+		
+		String response = given().queryParam("access_token", accessToken)
+		.when().log().all()
 		.get("https://rahulshettyacademy.com/getCourse.php").asString();
 		System.out.println(response);
 		
