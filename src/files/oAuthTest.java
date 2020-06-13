7bc2fc62de476b114362036ff5c2e62c9bb1029c
rizcube <rizcubeservices@gmail.com>
2020-06-13 14:39:42 +0100

    code is now extracted and passed in the query param

--- src/files/oAuthTest.java
+++ src/files/oAuthTest.java
@@ -8,7 +8,18 @@ public class oAuthTest {
 	
 	public static void main (String[] args) 
 	{
-		String accessTokenResponse  =given().queryParams("code","")
+		// first hit https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.email&auth_url=https://accounts.google.com/o/oauth2/v2/auth&client_id=692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com&response_type=code&redirect_uri=https://rahulshettyacademy.com/getCourse.php&state=abcd			
+					
+		String url = "https://rahulshettyacademy.com/getCourse.php?state=abcd&code=4%2F0wGYWO4E8DqBgeIYnSqVPoPmJ2PuGn9kGqQo-IHPCznOGJdrqs8Xoclsn0dD6cSmBTWFMMNB3jv45gEtSViCNgc&scope=email+openid+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&authuser=0&prompt=none#";
+		
+		String partialcode =url.split("code=")[1];
+		String code = partialcode.split("&scope")[0];
+		System.out.println("code is >"+code);
+		
+		
+		
+		String accessTokenResponse  =given().urlEncodingEnabled(false)
+		.queryParams("code",code)
 		.queryParams("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
 		.queryParams("client_secret", "erZOWM9g3UtwNRj340YYaK_W")
 		.queryParams("redirect_uri", "https://rahulshettyacademy.com/getCourse.php")
