9335387041caea4f7383f0158795a1cad0a15fb6
rizcube <rizcubeservices@gmail.com>
2020-06-15 14:33:28 +0100

    added base URI, and import static io.restassured.RestAssured.given;

--- src/pojo/SerializeTest.java
+++ src/pojo/SerializeTest.java
@@ -1,10 +1,13 @@
 package pojo;
 
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.given;
 public class SerializeTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	RestAssured.baseURI = "https://rahulshettyacademy.com";
+	
 	}
 
 }
