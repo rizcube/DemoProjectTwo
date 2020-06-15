a4b43508bf3d4cde830e3ae63789b909cbe23a08
rizcube <rizcubeservices@gmail.com>
2020-06-15 14:50:02 +0100

    Step 3: wrapped in a variable and printed response

--- src/pojo/SerializeTest.java
+++ src/pojo/SerializeTest.java
@@ -1,17 +1,37 @@
 package pojo;
 
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
 import static io.restassured.RestAssured.given;
 public class SerializeTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	RestAssured.baseURI = "https://rahulshettyacademy.com";
-	given().queryParam("key", "qaclick")
-	.body(arg0)
+	
+	Response res = given().queryParam("key", "qaclick")
+	.body("{\n" + 
+			"  \"location\": {\n" + 
+			"    \"lat\": -38.383494,\n" + 
+			"    \"lng\": 33.427362\n" + 
+			"  },\n" + 
+			"  \"accuracy\": 50,\n" + 
+			"  \"name\": \"Princess Castle 3\",\n" + 
+			"  \"phone_number\": \"(+91) 983 893 3937\",\n" + 
+			"  \"address\": \"Spiritual Landing place\",\n" + 
+			"  \"types\": [\n" + 
+			"    \"shoe park\",\n" + 
+			"    \"shop\"\n" + 
+			"  ],\n" + 
+			"  \"website\": \"http://google.com\",\n" + 
+			"  \"language\": \"French-IN\"\n" + 
+			"}")
 	.when().post("/maps/api/place/add/json")
 	.then().assertThat().statusCode(200).extract().response();
 	
+	String responseString = res.asString();
+	System.out.println(responseString);
 	}
 
 }
