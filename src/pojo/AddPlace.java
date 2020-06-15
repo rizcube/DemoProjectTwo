b29e67f5b80f80eedc0d6f9c415aaac4de437cf4
rizcube <rizcubeservices@gmail.com>
2020-06-15 15:07:46 +0100

    Defined variable given in the json

--- src/pojo/AddPlace.java
+++ src/pojo/AddPlace.java
@@ -1,5 +1,16 @@
 package pojo;
 
+import java.util.List;
+
 public class AddPlace {
+	
+	private String location;
+	private String accuracy;
+	private String name;
+	private String phone_number;
+	private String address;
+	private String types;
+	private String website;
+	private String language;
 
 }
