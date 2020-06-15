55a84652023674ebbd74198a9148f09799980ade
rizcube <rizcubeservices@gmail.com>
2020-06-15 14:43:54 +0100

    Step 2: added given(), when(), then() basic code

--- src/pojo/SerializeTest.java
+++ src/pojo/SerializeTest.java
@@ -7,6 +7,10 @@ public class SerializeTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	RestAssured.baseURI = "https://rahulshettyacademy.com";
+	given().queryParam("key", "qaclick")
+	.body(arg0)
+	.when().post("/maps/api/place/add/json")
+	.then().assertThat().statusCode(200).extract().response();
 	
 	}
 
