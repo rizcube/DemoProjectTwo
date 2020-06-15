048b03d3ddc07577f0472302f358ab65f6f4ff99
rizcube <rizcubeservices@gmail.com>
2020-06-15 15:26:05 +0100

    created getters and setters, in Serialize test created an object AddPlace p = new AddPlace();

--- src/pojo/Location.java
+++ src/pojo/Location.java
@@ -2,4 +2,6 @@ package pojo;
 
 public class Location {
 
+	private double lat;
+	private double lng;
 }
--- src/pojo/SerializeTest.java
+++ src/pojo/SerializeTest.java
@@ -10,23 +10,11 @@ public class SerializeTest {
 		// TODO Auto-generated method stub
 	RestAssured.baseURI = "https://rahulshettyacademy.com";
 	
+	AddPlace p = new AddPlace();
+	
+	
 	Response res = given().queryParam("key", "qaclick")
-	.body("{\n" + 
-			"  \"location\": {\n" + 
-			"    \"lat\": -38.383494,\n" + 
-			"    \"lng\": 33.427362\n" + 
-			"  },\n" + 
-			"  \"accuracy\": 50,\n" + 
-			"  \"name\": \"Princess Castle 3\",\n" + 
-			"  \"phone_number\": \"(+91) 983 893 3937\",\n" + 
-			"  \"address\": \"Spiritual Landing place\",\n" + 
-			"  \"types\": [\n" + 
-			"    \"shoe park\",\n" + 
-			"    \"shop\"\n" + 
-			"  ],\n" + 
-			"  \"website\": \"http://google.com\",\n" + 
-			"  \"language\": \"French-IN\"\n" + 
-			"}")
+	.body()
 	.when().post("/maps/api/place/add/json")
 	.then().assertThat().statusCode(200).extract().response();
 	
